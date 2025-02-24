a345c44a479a4b38676a1de570b5b555ede3c10d
ldtr96 <129347909+ldtr96@users.noreply.github.com>
2025-02-23 19:41:58 -0500

    Complete more JS exercises.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,42 @@
-const sumAll = function() {
+const sumAll = function(begin,end) {
+    if ((begin > 0 && end > 0) && (Number.isInteger(begin) && Number.isInteger(end))){
 
+    if (begin < end ){
+    const counterArrs = []
+    let sum = 0
+    for (let i = begin;i<= end;i++){
+        counterStart = i;
+        
+        counterArrs.push(counterStart)
+    }
+
+    
+    for (let t = 0; t < counterArrs.length; t++){
+        sum += counterArrs[t]
+    }
+
+    return sum;
+    } else if (begin > end){
+        const counterArrs = []
+        let sum = 0
+        for (let i = end;i<= begin;i++){
+            counterStart = i;
+            
+            counterArrs.push(counterStart)
+        }
+    
+        
+        for (let t = 0; t < counterArrs.length; t++){
+            sum += counterArrs[t]
+        }
+    
+        return sum;
+    }
+} else {
+    return 'ERROR'
 };
+
+}
 
 // Do not edit below this line
 module.exports = sumAll;
