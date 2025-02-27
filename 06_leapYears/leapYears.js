9ac44382461b940a9c05937cef2fa24d4c2eba8b
ldtr96 <129347909+ldtr96@users.noreply.github.com>
2025-02-26 21:52:35 -0500

    Complete the Leap Year exercise.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,15 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divFour = year % 4 == 0;
+    const divHundred = year % 100 ==0;
+    const divFourHundred = year % 400 ==0;
+    let outcome = ""
 
+        if (divHundred && divFourHundred){
+            outcome = true
+        } else if (divFour && !divHundred){
+            outcome = true
+        } else outcome = false
+    return outcome;
 };
 
 // Do not edit below this line
