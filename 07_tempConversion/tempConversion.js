3e1168676d8762644507e667e369a8e052fa68cc
ldtr96 <129347909+ldtr96@users.noreply.github.com>
2025-03-01 14:05:01 -0500

    Complete temp conversion JS exercise.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,22 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degrees) {
+
+  let convertedDegressC = (degrees - 32)*(5/9);
+
+  let roundConvertedDegreesC = Number(convertedDegressC.toFixed(1));
+
+  return roundConvertedDegreesC;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degrees) {
+
+  let convertedDegressF = degrees * (9/5) + 32;
+
+  let roundConvertedDegreesF = Number(convertedDegressF.toFixed(1));
+
+  return roundConvertedDegreesF;
+
+
 };
 
 // Do not edit below this line
